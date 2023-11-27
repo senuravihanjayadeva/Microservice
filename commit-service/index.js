5939934d757d449770f3464d382e56c85d8e16c6
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-11-27 21:30:23 +0530

    connect with aws dynamodb

--- commit-service/index.js
+++ commit-service/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const axios = require('axios');
+const { addCommit, getCommits } = require('./dynamodb');
 const app = express();
 const port = process.env.PORT | 3000;
 
@@ -7,10 +8,21 @@ app.get('/', (req, res) => {
     res.json({health:"Commit Service UP"})
 })
 
-app.get('/api/commits/:username/:repository', (req, res) => {
+app.get('/api/commits', (req, res) => {
+  getCommits().then((commits)=>{
+    res.json(commits)
+  }).catch((error)=>{
+    res.json({error: error});
+  })
+})
 
+app.get('/api/commits/:username/:repository', (req, res) => {
   axios.get(`https://api.github.com/repos/${req.params.username}/${req.params.repository}/commits`).then((response)=>{
-    res.json(response.data)
+    addCommit(response.data).then(()=>{
+      res.json(response.data)
+    }).catch((err)=>{
+      res.json({error:err})
+    })
   }).catch((e)=>{
     res.json({message:e.message})
   })
