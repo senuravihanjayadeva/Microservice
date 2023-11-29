5df670341cb72263ce0726b9f2dec1c94ea8401a
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-11-29 22:33:39 +0530

    ssss

--- commit-service/index.js
+++ commit-service/index.js
@@ -1,32 +1,5 @@
-const express = require('express');
-const axios = require('axios');
-const { addCommit, getCommits } = require('./dynamodb');
-const app = express();
-const port = process.env.PORT | 3000;
-
-app.get('/', (req, res) => {
-    res.json({health:"Commit Service UP"})
-})
-
-app.get('/api/commits', (req, res) => {
-  getCommits().then((commits)=>{
-    res.json(commits)
-  }).catch((error)=>{
-    res.json({error: error});
-  })
-})
-
-app.get('/api/commits/:username/:repository', (req, res) => {
-  axios.get(`https://api.github.com/repos/${req.params.username}/${req.params.repository}/commits`).then((response)=>{
-    addCommit(response.data).then(()=>{
-      res.json(response.data)
-    }).catch((err)=>{
-      res.json({error:err})
-    })
-  }).catch((e)=>{
-    res.json({message:e.message})
-  })
-})
+const app = require('./app');
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Commit service app listening on port ${port}`)
