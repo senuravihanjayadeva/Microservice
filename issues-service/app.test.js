d57c6f068dec9dc70f42ee81ae256453e6f45ac9
senuravihanjayadeva <senurajayadeva@gmail.com>
2023-11-29 22:29:23 +0530

    sd

--- issues-service/app.test.js
+++ issues-service/app.test.js
@@ -3,7 +3,7 @@ const app = require('./app'); // Assuming your Express app setup is in app.js or
 
 describe('Test endpoints', () => {
   it('responds with health message on /', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('/api/issues/senuravihanjayadeva/coding-challange');
     expect(response.status).toBe(200);
   });
 });
